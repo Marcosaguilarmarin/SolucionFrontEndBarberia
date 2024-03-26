2cfe4953637fcb353490d221d84ff7b2f5b66fd8
Diego Andrés Robles Monge <diego.anrm07@gmail.com>
2024-03-25 22:55:12 -0600

    header finalizado
    El header y la parte responsive esta finalizada, solo falta el footer

--- FrontEndBarberia/wwwroot/js/site.js
+++ FrontEndBarberia/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+const toggleNavul = document.querySelector(".toggleList");
+const NavMenu = document.querySelector(".navul")
+
+toggleNavul.addEventListener("click", () => {
+
+    NavMenu.classList.toggle("navul-visible")
+
+})
+
+
+/*
+document.addEventListener("DOMContentLoaded", function () {
+    var toggleListButton = document.getElementById("toggleListButton");
+    var navList = document.querySelector(".navul");
+
+    toggleListButton.addEventListener("click", function () {
+        if (navList.classList.contains("navul-hidden")) {
+            navList.classList.remove("navul-hidden");
+        } else {
+            navList.classList.add("navul-hidden");
+        }
+    });
+});*/
